7fdc3014224d54a5750e336a43be9a1797f45a7d
rpone <rponeawa@qq.com>
2026-09-12 01:07:40 +0800

    convert Mega links to megatools format

--- scripts/mega-link.mjs
+++ scripts/mega-link.mjs
@@ -1,4 +1,4 @@
 import { File } from 'megajs';
 const folder=File.fromURL(process.env.MEGA_FOLDER_URL); await folder.loadAttributes();
 const xs=(folder.children||[]).filter(x=>/秋城落叶/.test(decodeURIComponent(x.name||''))).filter(x=>!/(底包|base|firmware|official)/i.test(x.name||'')).sort((a,b)=>(b.timestamp||0)-(a.timestamp||0));
-if(!xs.length) throw Error('No ROM'); console.log(JSON.stringify({name:decodeURIComponent(xs[0].name),url:await xs[0].link({noKey:false})}));
+if(!xs.length) throw Error('No ROM'); const modern=await xs[0].link({noKey:false}); const m=modern.match(/\/file\/[^,]+,([^#]+)#(.+)$/); if(!m) throw Error('Unexpected Mega link '+modern); console.log(JSON.stringify({name:decodeURIComponent(xs[0].name),url:`https://mega.nz/#!${m[1]}!${m[2]}`}));
